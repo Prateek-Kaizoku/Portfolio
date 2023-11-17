a0f7ef4c94a56c536d6189396de40dfb8b852071
Prateek-Kaizoku <pra8335@gmail.com>
2023-11-17 00:50:43 +0000

    tweaks and changes

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -11,16 +11,20 @@ const Contact = () => {
       </div>
       <ul className={styles.links}>
         <li className={styles.link}>
-          <img src={getImageUrl("contact/emailIcon.png")} alt="github" />
-          <a href="mailto:myemail@email.com">myemail@gmail.com</a>
+          <a href="mailto:PrateekReachMe@gmail.com">
+            <img src={getImageUrl("contact/emailIcon.png")} alt="Email" />
+          </a>
         </li>
         <li className={styles.link}>
-          <img src={getImageUrl("contact/linkedinIcon.png")} alt="github" />
-          <a href="mailto:myemail@email.com">myemail@gmail.com</a>
+          <a href="https://www.linkedin.com/in/contact-prateek">
+            {" "}
+            <img src={getImageUrl("contact/linkedinIcon.png")} alt="Linkedin" />
+          </a>
         </li>
         <li className={styles.link}>
-          <img src={getImageUrl("contact/githubIcon.png")} alt="github" />
-          <a href="mailto:myemail@email.com">myemail@gmail.com</a>
+          <a href="https://github.com/Prateek-Kaizoku">
+            <img src={getImageUrl("contact/githubIcon.png")} alt="github" />
+          </a>
         </li>
       </ul>
     </footer>
